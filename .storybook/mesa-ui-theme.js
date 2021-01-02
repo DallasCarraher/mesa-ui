0b3817895909ff4e10d2f2fb2dafe04f31b41d6c
DallasCarraher <dallas.carraher@gmail.com>
2021-01-01 20:12:46 -0800

    quickstart for our own theme

---
+++ .storybook/mesa-ui-theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Mesa-UI',
+  brandUrl: 'https://github.com/dallascarraher/mesa-ui',
+  brandImage: 'https://i.imgur.com/I3qCsSf.jpeg',
+})
