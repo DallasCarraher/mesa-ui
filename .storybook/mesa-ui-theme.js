b73e6082dc32c7c77f431e1e9a6927398f7d0ec4
DallasCarraher <dallas.carraher@gmail.com>
2021-01-01 20:15:43 -0800

    quick note on theming

--- .storybook/mesa-ui-theme.js
+++ .storybook/mesa-ui-theme.js
@@ -1,5 +1,8 @@
 import { create } from '@storybook/theming/create'
 
+// We'll look into theming more later
+// https://storybook.js.org/docs/react/configure/theming
+
 export default create({
   base: 'dark',
   brandTitle: 'Mesa-UI',
