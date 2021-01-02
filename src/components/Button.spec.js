1c2b6789507a7281562d9033bc86a4f04c8e003f
DallasCarraher <dallas.carraher@gmail.com>
2021-01-02 14:02:23 -0800

    new test for button prop text

--- src/components/Button.spec.js
+++ src/components/Button.spec.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Button from './Button'
+import { Button } from './Button'
 
 describe('Button', () => {
-  test('onClick action', () => {
+  test('text', () => {
+    render(<Button text="Button" />)
+    const ourButton = screen.getByRole('button', { name: 'Button' })
+    expect(ourButton).toBeInTheDocument()
+  })
+
+  test('onClick', () => {
     const clickHandler = jest.fn()
     render(<Button text="Button" onClick={clickHandler} />)
     userEvent.click(screen.getByRole('button', { name: 'Button' }))
