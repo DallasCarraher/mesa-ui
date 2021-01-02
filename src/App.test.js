43abecb062c3624932dcd9050e5262186a88f744
DallasCarraher <dallas.carraher@gmail.com>
2021-01-01 17:29:06 -0800

    remove create-react-app

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('Development Page Renders', () => {
-  render(<App />)
-  const message = screen.getByText(/Development Server Running/)
-  expect(message).toBeInTheDocument()
-})
